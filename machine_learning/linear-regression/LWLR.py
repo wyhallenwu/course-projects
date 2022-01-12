ebbec2ba75808b22a0ad303780285cd731f62113
wyhallenwu <wyhforwork66@gmail.com>
2022-01-12 21:14:24 +0800

    add sklearn method

---
+++ machine_learning/linear-regression/LWLR.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def loadDataSet(fileName):
+    f = open('./dataset.txt')
+    numFeatures = len(f.readline().split('\t')) - 1
+    xArr = []
+    yArr = []
+    fr = open('./dataset.txt')
+    for line in fr.readlines():
+        xLine = []
+        curline = line.strip().split('\t')
+        for i in range(numFeatures):
+            xLine.append(float(curline[i]))
+        xArr.append(xLine)
+        yArr.append(float(curline[-1]))
+    f.close()
+    fr.close()
+    return xArr,yArr
+
+
+def lwlr(xTest,xArr,yArr,k = 0.1):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    n = np.shape(xArr)[0]      # number of data pairs
+    yPredict = np.zeros(n)
+    # compute w and theta for each point
+    for point in range(n):
+
+        w = np.mat(np.eye((n)))           # weight matrix
+
+        # compute weight matrix for single point
+        for i in range(n):
+            difMat = xTest[point,:] - xMat[i,:]
+            w[i,i] = np.exp(difMat * difMat.T / (-2 * k * k))
+
+        # compute theta for the point
+        xTwx = xMat.T * (w * xMat)
+        if np.linalg.det(xTwx) == 0:
+            print('sigular matrix cant get inverse')
+            return
+        theta = xTwx.I * (xMat.T * (w * yMat))
+        yPredict[point] = xTest[point,:] * theta        # predictions
+
+    return yPredict
+
+
+def plotLWLR(xArr,yArr,xTest,yPredict1,yPredict2,yPredict3):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr)
+    srtIdx = xTest[:, 1].argsort(0)
+    xSort = xTest[srtIdx][:,0,:]
+
+    fig, axs = plt.subplots(nrows=3, ncols=1, sharex=False, sharey=False, figsize=(10, 8))
+    axs[0].plot(xSort[:, 1], yPredict1[srtIdx], c='red')  
+    axs[1].plot(xSort[:, 1], yPredict2[srtIdx], c='red')  
+    axs[2].plot(xSort[:, 1], yPredict3[srtIdx], c='red')
+    axs[0].scatter(xMat[:, 1].flatten().A[0], yMat.flatten().A[0], s=20, c='g', alpha=.5) 
+    axs[1].scatter(xMat[:, 1].flatten().A[0], yMat.flatten().A[0], s=20, c='g', alpha=.5)  
+    axs[2].scatter(xMat[:, 1].flatten().A[0], yMat.flatten().A[0], s=20, c='g', alpha=.5)  
+    axs0_title_text = axs[0].set_title(u'LWLR,k=1.0')
+    axs1_title_text = axs[1].set_title(u'LWLR,k=0.01')
+    axs2_title_text = axs[2].set_title(u'LWLR,k=0.003')
+    plt.setp(axs0_title_text, size=8, weight='bold', color='red')
+    plt.setp(axs1_title_text, size=8, weight='bold', color='red')
+    plt.setp(axs2_title_text, size=8, weight='bold', color='red')
+    plt.xlabel('X')
+    plt.show()
+
+def pipeline(filename):
+    xArr ,yArr = loadDataSet(filename)
+    xMat = np.mat(xArr)
+    yP1 = lwlr(xMat,xArr,yArr,k=1)
+    yP2 = lwlr(xMat,xArr,yArr,k=0.01)
+    yP3 = lwlr(xMat,xArr,yArr,k=0.003)
+    plotLWLR(xArr,yArr,xMat,yP1,yP2,yP3)
+
+if __name__ == '__main__':
+    pipeline('./dataset.txt')
---
+++ machine_learning/linear-regression/linear_regression.py
@@ -0,0 +1,75 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from my_module.mymodule import get_exec_time
+
+# dataloader: extract data from dataset
+def loadData(filename):
+    data = np.genfromtxt(filename)
+    print(data.shape)
+    x = data[:, 1]
+    y = data[:, 2]
+    return x, y
+
+# depict the data
+def dataPlot(filename):
+    x, y = loadData(filename)    
+    p = plt.figure()
+    plt.scatter(x, y, c='g', alpha=0.5)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title("dataset")
+    plt.show()
+
+# gradient descent iteration
+def gradient_descent_iter(x, y):
+    # define hyperparameter
+    learning_rate = 0.001
+    iter_num = 10000
+    m = 0
+    b = 0
+    num = len(x)
+    # iteration 
+    for i in range(iter_num):
+        totalError = compute_total_error(x, y, m, b)
+        # uptate parameters m, b
+        m, b = step_gradient_descent(m, b, learning_rate, x, y)
+    return m, b
+
+# compute the error using MSE, return float
+def compute_total_error(x, y, current_m, current_b):
+    totalError = 0
+    for i in range(len(x)):
+        totalError += (y[i] - (current_m * x[i] + current_b)) ** 2
+    return totalError / float(len(x))
+
+# single step of gradient descent, return steped_m and steped_b
+def step_gradient_descent(current_m, current_b, learning_rate, x, y):
+    m_gradient = 0
+    b_gradient = 0
+    N = float(len(x))
+    for i in range(len(x)):
+        b_gradient += -(2 / N) * (y[i] - (current_m * x[i] + current_b))
+        m_gradient += -(2 / N) * x[i] * (y[i] - ((current_m * x[i]) + current_b))
+    step_m = current_m - (learning_rate * m_gradient)
+    step_b = current_b - (learning_rate * b_gradient)
+    return step_m, step_b
+
+# the training pipeline of linear regression
+@get_exec_time
+def gradient_run(filename):
+    print("running...")
+    x, y = loadData(filename)
+    m, b = gradient_descent_iter(x, y)
+    print("the coefficient is: {:.2f} \nthe bias is: {:.2f}".format(m, b))
+    xd = np.arange(0, 1, 0.005)
+    yd = m * xd + b
+    plt.scatter(x, y, c='g', alpha=0.5)
+    plt.plot(xd, yd, c='r')
+    plt.show()
+
+
+if __name__ == '__main__':
+    # dataPlot('linear-regression/dataset.txt')
+    gradient_run('machine learning/linear-regression/dataset.txt')
+     
